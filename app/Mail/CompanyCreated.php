cd020e1931167a055653ba3ed469afee376548e8
Kevin Peña <kevin.pena.prog@gmail.com>
2019-07-23 02:41:31 -0500

    Add CompanyCreated Mailable

---
+++ app/Mail/CompanyCreated.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class CompanyCreated extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    private $empresa;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($empresa)
+    {
+        $this->empresa = $empresa;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->subject('Nueva empresa creada')->view('emails.companycreated');
+    }
+}
---
+++ resources/views/emails/companycreated.blade.php
@@ -0,0 +1,6 @@
+<p>Se ha creado una nueva empresa:</p>
+<ul>
+    <li>Nombre: {{$empresa->name}}</li>
+    <li>Correo: {{$empresa->email}}</li>
+    <li>Sitio web: {{$empresa->website}}</li>
+</ul>
