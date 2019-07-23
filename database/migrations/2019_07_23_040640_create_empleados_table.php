4243d19708990fc31e54519675050ae229dbc5ad
Kevin Peña <kevin.pena.prog@gmail.com>
2019-07-22 23:11:43 -0500

    Add Empleado model and migration

---
+++ database/migrations/2019_07_23_040640_create_empleados_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEmpleadosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('empleados', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->unsignedBigInteger('company_id');
+            $table->string('email')->nullable();
+            $table->string('phone')->nullable();
+
+            $table->foreign('company_id')->references('id')->on('empresas');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('empleados');
+    }
+}
